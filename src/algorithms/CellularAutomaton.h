c5951c49ea6a2df5e744aa37847f5de207034737
Juan-Antonio <cyberjuen@gmail.com>
2014-10-12 00:16:19 +0000

    Added more kernels. Not tested yet.

--- src/algorithms/CellularAutomaton.h
+++ src/algorithms/CellularAutomaton.h
@@ -15,7 +15,11 @@ public:
     CellularAutomaton(clever::context & context) :
         KernelWrapper(context),
         iteration(context),
-        memset(context)
+        memset(context),
+        memcpy(context),
+        countFollowerBasis(context),
+        storeFollowerBasis(context),
+        determineFollowerBestBasis(context)
     {
     }
     
@@ -80,5 +84,129 @@ public:
         memory[gid] = value;
     },
     cl_mem, cl_uint,
+    cl_uint);
+
+    KERNEL_CLASS(memcpy,
+        __kernel void memcpy(
+            //input
+            const __global uint * const __restrict src,
+            //output
+            __global uint * const __restrict dst,
+            //workload
+            const uint length)
+    {
+        const size_t gid = get_global_id(0);
+
+        if (gid >= length) {
+            return;
+        }
+
+        dst[gid] = src[gid];
+    },
+    cl_mem,
+    cl_mem,
+    cl_uint);
+
+    KERNEL_CLASS(countFollowerBasis,
+        __kernel void countFollowerBasis(
+            //input
+            const __global uint * const __restrict tripletsBasis,
+            const __global uint * const __restrict tripletsFollowers,
+            const __global uint * const __restrict tripletsStatus,
+            //output
+            __global uint * const __restrict followerBasisCount,
+            //workload
+            const uint nTripletPairs)
+    {
+        const size_t tripletPair = get_global_id(0);
+
+        if (tripletPair >= nTripletPairs) {
+            return;
+        }
+        //this could be stored on a vector to have the offsets already and do not need
+        //to call atomic_inc again in the store phase
+        const uint followerTriplet = tripletsFollowers[tripletPair];
+        if (tripletsStatus[tripletsBasis[tripletPair]] + 1 != tripletsStatus[followerTriplet]){
+            return;
+        }
+
+        atomic_inc(&(followerBasisCount[followerTriplet]));
+    },
+    cl_mem, cl_mem, cl_mem,
+    cl_mem,
+    cl_uint);
+
+    KERNEL_CLASS(storeFollowerBasis,
+        __kernel void storeFollowerBasis(
+            //input
+            const __global uint * const __restrict tripletsBasis,
+            const __global uint * const __restrict tripletsFollowers,
+            const __global uint * const __restrict tripletsStatus,
+            const __global float * const __restrict tripletsPt,
+            //I/O, but the output is useless
+            __global uint * const __restrict followerBasisCountPrefixSum,
+            //output
+            __global float * const __restrict ptDiff,
+            __global uint * const __restrict basisIndexes,
+            //workload
+            const uint nTripletPairs)
+    {
+        const size_t tripletPair = get_global_id(0);
+
+        if (tripletPair >= nTripletPairs) {
+            return;
+        }
+
+        const uint tripletBasis = tripletsBasis[tripletPair];
+        const uint tripletFollower = tripletsFollowers[tripletPair];
+                
+        if (tripletsStatus[tripletBasis] + 1 != tripletsStatus[tripletFollower]){
+            return;
+        }
+        
+        uint storeOffset = atomic_inc(&(followerBasisCountPrefixSum[tripletFollower]));
+        
+        ptDiff[storeOffset] = fabs(tripletsPt[tripletBasis] - tripletsPt[tripletFollower]);
+        basisIndexes[storeOffset] = tripletBasis;
+    },
+    
+    cl_mem, cl_mem, cl_mem, cl_mem, cl_mem,
+    cl_mem, cl_mem,
+    cl_uint);
+
+
+    KERNEL_CLASS(determineFollowerBestBasis,
+        __kernel void determineFollowerBestBasis(
+            //input
+            const __global uint * const __restrict followerBasisCountPrefixSum,
+            const __global float * const __restrict followerBasisPtDifference,
+            const __global uint * const __restrict basisIndexes,
+            //output
+            __global uint * const __restrict followerBestBasis,
+            //workload
+            const uint nTriplets)
+    {
+        const size_t tripletIndex = get_global_id(0);
+
+        if (tripletIndex >= nTriplets) {
+            return;
+        }
+
+        const uint begin = followerBasisCountPrefixSum[tripletIndex];
+        const uint end = followerBasisCountPrefixSum[tripletIndex + 1];
+        float minPt = followerBasisPtDifference[begin];
+        uint minPtIndex = basisIndexes[begin];
+        
+        for(uint i = begin + 1; i < end; i++){
+            float ptDiff = followerBasisPtDifference[i];
+            bool test = ptDiff < minPt;
+            minPt = minPt * !test + ptDiff * test;
+            minPtIndex =  minPtIndex * !test + basisIndexes[i] * test;
+        }
+        followerBestBasis[tripletIndex] = minPtIndex;
+    },
+    
+    cl_mem, cl_mem, cl_mem, cl_mem, cl_mem,
+    cl_mem, cl_mem,
     cl_uint);
 };
