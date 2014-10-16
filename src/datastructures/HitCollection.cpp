533064c6c7b01ee6ca9e94366bd5af80ea8abaa6
Juan-Antonio <cyberjuen@gmail.com>
2014-10-16 11:09:31 +0000

    Reformatted

--- src/datastructures/HitCollection.cpp
+++ src/datastructures/HitCollection.cpp
@@ -7,147 +7,168 @@ HitCollection::HitCollection(const std::vector<PB_Event::PEvent *>& events)
 {
     /*
     for ( auto e : events )
-	{
-		addEvent(*e, DetectorGeometry(), LayerSupplement(0 , 0));
-	}
+    {
+        addEvent(*e, DetectorGeometry(), LayerSupplement(0 , 0));
+    }
     */
-
 }
 
 HitCollection::HitCollection(const PB_Event::PEvent & event)
 {
-	//addEvent(event, DetectorGeometry(), LayerSupplement(0, 0));
+    //addEvent(event, DetectorGeometry(), LayerSupplement(0, 0));
 }
 
-HitCollection::tTrackList HitCollection::addEvent(const PB_Event::PEvent& event, const DetectorGeometry & geom, EventSupplement & eventSupplement, uint evtInGroup, LayerSupplement & layerSupplement ,
-		const TripletConfigurations & layerTriplets, int numTracks, bool onlyTracks) {
+HitCollection::tTrackList HitCollection::addEvent(const PB_Event::PEvent& event,
+        const DetectorGeometry & geom, EventSupplement & eventSupplement, uint evtInGroup,
+        LayerSupplement & layerSupplement ,
+        const TripletConfigurations & layerTriplets, int numTracks, bool onlyTracks)
+{
+    float minPt = layerTriplets.minPt_;
+    std::set<uint> usedLayers;
 
-	float minPt = layerTriplets.minPt_;
-	std::set<uint> usedLayers;
+    uint maxLayer = 0;
 
-	uint maxLayer = 0;
+    for (uint i = 0; i < layerTriplets.size(); ++i) {
+        usedLayers.insert(layerTriplets[i].layer1());
+        usedLayers.insert(layerTriplets[i].layer2());
+        usedLayers.insert(layerTriplets[i].layer3());
 
-	for(uint i = 0; i < layerTriplets.size(); ++i){
-		usedLayers.insert(layerTriplets[i].layer1());
-		usedLayers.insert(layerTriplets[i].layer2());
-		usedLayers.insert(layerTriplets[i].layer3());
+        if (layerTriplets[i].layer3() > maxLayer) {
+            maxLayer = layerTriplets[i].layer3();
+        }
+    }
 
-		if(layerTriplets[i].layer3() > maxLayer)
-			maxLayer = layerTriplets[i].layer3();
-	}
+    //associate hits to tracks
+    tTrackList allTracks;
+    for (auto& hit : event.hits()) {
+        if (onlyTracks && hit.simtrackid() == 0) {
+            continue;
+        }
 
-	//associate hits to tracks
-	tTrackList allTracks;
-	for (auto& hit : event.hits()) {
+        if (hit.simtrackpt() < minPt) {
+            continue;
+        }
 
-		if(onlyTracks && hit.simtrackid() == 0)
-			continue;
+        allTracks[hit.simtrackid()].push_back(hit);
+    }
 
-		if (hit.simtrackpt() < minPt)
-			continue;
+    //sort hits of tracks by layer
+    for (auto & track : allTracks)
+        std::sort(track.second.begin(), track.second.end(),
+        [](const PB_Event::PHit & a, const PB_Event::PHit & b) {
+        return a.layer() < b.layer();
+    });
+ 	
+ 	// hits bucket sorted into layers
+    tTrackList layers;
+    //findable tracks = collection of tracks that count towards efficiency
+    tTrackList findableTracks;
+    //found tracks initialized to 0; if no limit on track, i.e. numTracks=-1, found track will never be equal to numTracks
+    int foundTracks = 0;
+    
+    for (tTrackList::const_iterator itTrack = allTracks.begin(); itTrack != allTracks.end()
+            && foundTracks != numTracks; ++itTrack) {
+        //skip unassociated hits
+        if (onlyTracks && itTrack->first == 0) {
+            continue;
+        }
 
-		allTracks[hit.simtrackid()].push_back(hit);
-	}
+        //check for missing hits
+        //bool covLayers[maxLayer]; -> commented to not use varLength arrays
+        std::vector< bool > covLayers(maxLayer);
+        for (auto hit : itTrack->second) {
+            covLayers[hit.layer() - 1] = true;
+        }
+     
+        bool missingHits = false;
+        for (auto l : usedLayers) {
+            if (!covLayers[l - 1]) {
+                missingHits = true;
+                //std::cout << "Track: " << itTrack->first << " missing hit in " << l-1 << std::endl;
+            }
+        }
 
-	//sort hits of tracks by layer
-	for(auto & track : allTracks)
-		std::sort(track.second.begin(), track.second.end(),
-				[] (const PB_Event::PHit & a, const PB_Event::PHit & b)
-				{
-					return a.layer() < b.layer();
-				});
+        //skip tracks with to low pt
+        if (itTrack->second[0].simtrackpt() < minPt) {
+            continue;
+        }
 
-	tTrackList layers; // hits bucket sorted into layers
-	tTrackList findableTracks; //findable tracks = collection of tracks that count towards efficiency
-	int foundTracks = 0; //found tracks initialized to 0; if no limit on track, i.e. numTracks=-1, found track will never be equal to numTracks
-	for(tTrackList::const_iterator itTrack = allTracks.begin(); itTrack != allTracks.end() && foundTracks != numTracks; ++itTrack){
-		//skip unassociated hits
-		if(onlyTracks && itTrack->first == 0)
-			continue;
+        //add all hits to track
+        for (auto & hit : itTrack->second) {
+            if (!missingHits) {
+                findableTracks[itTrack->first].push_back(hit);
+            } else if (!onlyTracks) {
+                layers[hit.layer()].push_back(hit);
+            }
+        }
 
-		//check for missing hits
-		//bool covLayers[maxLayer]; -> commented to not use varLength arrays
-        std::vector< bool > covLayers( maxLayer );
-		for(auto hit : itTrack->second){
-			covLayers[hit.layer()-1] = true;
-		}
-		bool missingHits = false;
-		for(auto l : usedLayers){
-			if(!covLayers[l-1]){
-				missingHits = true;
-				//std::cout << "Track: " << itTrack->first << " missing hit in " << l-1 << std::endl;
-			}
-		}
+        if (!missingHits) {
+            ++foundTracks;
+        }
+    }
 
-		//skip tracks with to low pt
-		if(itTrack->second[0].simtrackpt() < minPt)
-			continue;
+    //bucket sort findable trakc hits into layers
+    for (tTrackList::const_iterator itTrack = findableTracks.begin();
+            itTrack != findableTracks.end(); ++itTrack) {
+        for (auto& hit : itTrack->second) {
+            if (hit.layer() <= maxLayer) {
+                layers[hit.layer()].push_back(hit);
+            }
+        }
+    }
 
-		//add all hits to track
-		for(auto & hit : itTrack->second){
-			if(!missingHits)
-				findableTracks[itTrack->first].push_back(hit);
-			else if(!onlyTracks)
-				layers[hit.layer()].push_back(hit);
-		}
+    //fill event and layer supplement
+    uint offset = 0;
+    for (uint i = 1; i <= maxLayer; ++i) {
+        layerSupplement[evtInGroup * maxLayer + i - 1].setNHits(layers[i].size());
+        layerSupplement[evtInGroup * maxLayer + i - 1].setOffset(offset);
+        offset += layers[i].size();
+    }
+    
+    //offset contains total number of hits for this event
+    eventSupplement[evtInGroup].setNHits(offset);
+    //use offset for evt offset computation
+    offset = evtInGroup > 0 ? eventSupplement[evtInGroup - 1].getOffset() +
+             eventSupplement[evtInGroup - 1].getNHits() : 0;
+    eventSupplement[evtInGroup].setOffset(offset);
 
-		if(!missingHits)
-			++foundTracks;
-	}
+    //add hits in order of layers to HitCollection
+    for (uint i = 1; i <= maxLayer; ++i) {
+        PLOG << "Layer " << i << std::endl;
 
-	//bucket sort findable trakc hits into layers
-	for(tTrackList::const_iterator itTrack = findableTracks.begin(); itTrack != findableTracks.end(); ++itTrack){
+        for (auto & hit : layers[i]) {
+            uint detId = hit.detectorid();
+            int index = geom.resolveDetId(detId);
+            if (index != -1) {
+                detId = index;
+            } else {
+                PLOG << "\tInvalid DetectorId used: << " << detId << std::endl;
+            }
 
-		for (auto& hit : itTrack->second) {
-			if(hit.layer() <= maxLayer){
-				layers[hit.layer()].push_back(hit);
-			}
-		}
-	}
+            int id = addWithValue(hit.position().x(), hit.position().y(),
+                                  hit.position().z(), hit.layer(), detId,
+                                  hit.simtrackid(), evtInGroup);
 
-	//fill event and layer supplement
-	uint offset = 0;
-	for(uint i = 1; i <= maxLayer; ++i){
-		layerSupplement[evtInGroup * maxLayer + i-1].setNHits(layers[i].size());
-		layerSupplement[evtInGroup * maxLayer + i-1].setOffset(offset);
-		offset += layers[i].size();
-	}
-	//offset contains total number of hits for this event
-	eventSupplement[evtInGroup].setNHits(offset);
-	//use offset for evt offset computation
-	offset = evtInGroup > 0 ? eventSupplement[evtInGroup-1].getOffset() + eventSupplement[evtInGroup-1].getNHits() : 0;
-	eventSupplement[evtInGroup].setOffset(offset);
+            PLOG << "\t [" << id << "] Track: " << hit.simtrackid() << " DetId: " << detId;
+            PLOG << "\t\t [" << hit.position().x() << ", " <<  hit.position().y() << ", " <<
+                 hit.position().z() << "]";
+            PLOG << std::endl;
+        }
+    }
 
+    uint i = 0;
+    for (tTrackList::const_iterator itTrack = findableTracks.begin();
+            itTrack != findableTracks.end(); ++itTrack) {
+        PLOG << "FTRACK #" << itTrack->first << " " << std::endl;
+        for (auto& hit : itTrack->second) {
+            PLOG << "\t[" << hit.position().x() << ' ' << hit.position().y() << ' ' << hit.position().z()
+                 << "]" << std::endl;
+        }
+        PLOG << std::endl;
+        i++;
+    }
 
-	//add hits in order of layers to HitCollection
-	for(uint i = 1; i <= maxLayer; ++i){
-
-
-		PLOG << "Layer " << i << std::endl;
-
-
-		for(auto & hit : layers[i]){
-
-			uint detId = hit.detectorid();
-		    int index = geom.resolveDetId(detId);
-		    if(index != -1)
-		    	detId = index;
-		    else
-		    	PLOG << "Invalid DetectorId used: << " << detId << std::endl;
-
-			int id = addWithValue(hit.position().x(), hit.position().y(),
-						 	 	   hit.position().z(), hit.layer(), detId,
-						 	 	   hit.simtrackid(), evtInGroup);
-
-
-			PLOG << "\t [" << id << "] Track: " << hit.simtrackid() << " DetId: " << detId;
-			PLOG << "\t\t [" << hit.position().x()<< ", " <<  hit.position().y()<< ", " <<  hit.position().z() << "]";
-			PLOG << std::endl;
-
-		}
-	}
-
-	return findableTracks;
+    return findableTracks;
 }
 
 HitCollection PHitWrapper::pHitCollection;
--- src/datastructures/HitCollection.h
+++ src/datastructures/HitCollection.h
@@ -28,70 +28,71 @@ typedef clever::Collection <HIT_COLLECTION_ITEMS> HitCollectionItems;
 class HitCollection: public HitCollectionItems
 {
 public:
-	typedef HitCollectionItems dataitems_type;
+    typedef HitCollectionItems dataitems_type;
+    //quickly access both ends of track + random access
+    typedef std::deque<PB_Event::PHit> tTrackHits;
+    //key = trackID, value trackDeque --> easy application of cuts
+    typedef std::map<uint, tTrackHits> tTrackList;
+    typedef tTrackList::value_type tTrackListEntry;
 
-	typedef std::deque<PB_Event::PHit> tTrackHits; //quickly access both ends of track + random access
-	typedef std::map<uint, tTrackHits> tTrackList; //key = trackID, value trackDeque --> easy application of cuts
-	typedef tTrackList::value_type tTrackListEntry;
+    HitCollection()
+    {
 
-	HitCollection()
-	{
+    }
 
-	}
+    HitCollection(int items) :
+        clever::Collection<HIT_COLLECTION_ITEMS>(items)
+    {
 
-	HitCollection(int items) :
-			clever::Collection<HIT_COLLECTION_ITEMS>(items)
-	{
+    }
 
-	}
+    // use a range of events to bootstrap the hit collection
+    HitCollection(const std::vector < PB_Event::PEvent * >& events) ;
 
-	// use a range of events to bootstrap the hit collection
-	HitCollection(const std::vector < PB_Event::PEvent * >& events) ;
+    HitCollection(const PB_Event::PEvent & event) ;
 
-	HitCollection(const PB_Event::PEvent & event) ;
-
-	tTrackList addEvent(const PB_Event::PEvent& event, const DetectorGeometry& geom, EventSupplement & eventSupplement, uint evtInGroup, LayerSupplement & layerSupplement,
-			const TripletConfigurations & layerTriplets, int numTracks = -1, bool onlyTracks = false) ;
+    tTrackList addEvent(const PB_Event::PEvent& event, const DetectorGeometry& geom,
+                        EventSupplement & eventSupplement, uint evtInGroup, LayerSupplement & layerSupplement,
+                        const TripletConfigurations & layerTriplets, int numTracks = -1, bool onlyTracks = false) ;
 
 public:
-	clever::OpenCLTransfer<HIT_COLLECTION_ITEMS> transfer;
-
+    clever::OpenCLTransfer<HIT_COLLECTION_ITEMS> transfer;
 };
 /*
 class GlobalPosition: private CollectionView<HitCollection>
 {
 public:
-	// get a pointer to one hit in the collection
-	GlobalPosition(HitCollection & collection, index_type i) :
-			CollectionView<HitCollection>(collection, i)
-	{
+    // get a pointer to one hit in the collection
+    GlobalPosition(HitCollection & collection, index_type i) :
+            CollectionView<HitCollection>(collection, i)
+    {
 
-	}
+    }
 
-	float x() const
-	{
-		return getValue<GlobalX>();
-	}
+    float x() const
+    {
+        return getValue<GlobalX>();
+    }
 
-	float y() const
-	{
-		return getValue<GlobalY>();
-	}
+    float y() const
+    {
+        return getValue<GlobalY>();
+    }
 
-	void setX(float v)
-	{
-		setValue<GlobalX>(v);
-	}
+    void setX(float v)
+    {
+        setValue<GlobalX>(v);
+    }
 
-	void setY(float v)
-	{
-		setValue<GlobalY>(v);
-	}
+    void setY(float v)
+    {
+        setValue<GlobalY>(v);
+    }
 
-	// * a bit difficult, where do we store to ?
-	 GlobalPosition & GlobalPosition::operator+=(const GlobalPosition &rhs) {
-	 rs
-	 }
+    // * a bit difficult, where do we store to ?
+     GlobalPosition & GlobalPosition::operator+=(const GlobalPosition &rhs) {
+     rs
+     }
 };
 */
 
@@ -99,87 +100,85 @@ class Hit: public clever::CollectionView<HitCollection>
 {
 public:
 // get a pointer to one hit in the collection
-	Hit(HitCollection & collection, index_type i) :
-			clever::CollectionView<HitCollection>(collection, i)
-	{
+    Hit(HitCollection & collection, index_type i) :
+        clever::CollectionView<HitCollection>(collection, i)
+    {
+        x = getValue<GlobalX>();
+        y = getValue<GlobalY>();
+        z = getValue<GlobalZ>();
+    }
 
-		x = getValue<GlobalX>();
-		y = getValue<GlobalY>();
-		z = getValue<GlobalZ>();
-
-	}
-
-	// read-only get a pointer to one hit in the collection
-	Hit(const HitCollection & collection, index_type i) :
-		clever::CollectionView<HitCollection>(collection, i)
-		{
-
-		x = getValue<GlobalX>();
-		y = getValue<GlobalY>();
-		z = getValue<GlobalZ>();
-
-		}
+    // read-only get a pointer to one hit in the collection
+    Hit(const HitCollection & collection, index_type i) :
+        clever::CollectionView<HitCollection>(collection, i)
+    {
+        x = getValue<GlobalX>();
+        y = getValue<GlobalY>();
+        z = getValue<GlobalZ>();
+    }
 
 // create a new hit in the collection
-	Hit(HitCollection & collection) :
-			clever::CollectionView<HitCollection>(collection)
-	{
-		x = 0;
-		y = 0;
-		z = 0;
-	}
+    Hit(HitCollection & collection) :
+        clever::CollectionView<HitCollection>(collection)
+    {
+        x = 0;
+        y = 0;
+        z = 0;
+    }
 
-	bool operator==(const Hit & rhs) const {
-		return getValue<EventNumber>() == rhs.getValue<EventNumber>()
-				&& getValue<HitId>() == rhs.getValue<HitId>()
-				&& getValue<DetectorId>() == rhs.getValue<DetectorId>()
-				&& getValue<DetectorLayer>() == rhs.getValue<DetectorLayer>()
-				&& globalX() == rhs.globalX() && globalY() == rhs.globalY() && globalZ() == rhs.globalZ(); //values are only copied ==> should be bitwise identical
-	}
+    bool operator==(const Hit & rhs) const
+    {
+        return getValue<EventNumber>() == rhs.getValue<EventNumber>()
+               && getValue<HitId>() == rhs.getValue<HitId>()
+               && getValue<DetectorId>() == rhs.getValue<DetectorId>()
+               && getValue<DetectorLayer>() == rhs.getValue<DetectorLayer>()
+               && globalX() == rhs.globalX() && globalY() == rhs.globalY()
+               && globalZ() == rhs.globalZ(); //values are only copied ==> should be bitwise identical
+    }
 
-	float globalX() const
-	{
-		return x;
-	}
+    float globalX() const
+    {
+        return x;
+    }
 
-	float globalY() const
-	{
-		return y;
-	}
+    float globalY() const
+    {
+        return y;
+    }
 
-	float globalZ() const
-	{
-		return z;
-	}
+    float globalZ() const
+    {
+        return z;
+    }
 
-	float phi() const {
-		return atan2(globalY(), globalX());
-	}
+    float phi() const
+    {
+        return atan2(globalY(), globalX());
+    }
 
-	float theta() const {
-		return atan2( sqrt(globalX()*globalX() + globalY()*globalY()) ,globalZ()*globalZ());
-	}
+    float theta() const
+    {
+        return atan2(sqrt(globalX() * globalX() + globalY() * globalY()) , globalZ() * globalZ());
+    }
 
 protected:
-	float x;
-	float y;
-	float z;
-
+    float x;
+    float y;
+    float z;
 };
 
-class PHitWrapper : public Hit {
-
+class PHitWrapper : public Hit
+{
 public:
 
-	PHitWrapper(const PB_Event::PHit & hit)
-	: Hit(pHitCollection)
-	{
-		x = hit.position().x();
-		y = hit.position().y();
-		z = hit.position().z();
-	}
+    PHitWrapper(const PB_Event::PHit & hit)
+        : Hit(pHitCollection)
+    {
+        x = hit.position().x();
+        y = hit.position().y();
+        z = hit.position().z();
+    }
 
 private:
-	static HitCollection pHitCollection;
-
+    static HitCollection pHitCollection;
 };
